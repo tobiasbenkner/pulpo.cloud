0c0a36dd6c3ff7b3a451caec93f35dc14ec6c7b2
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-21 19:53:48 +0000

    better typing

--- src/utils/getFile.ts
+++ src/utils/getFile.ts
@@ -1,12 +1,11 @@
 // Pattern: {pocketbase_url}/api/files/{collection_name}/{record_id}/{filename}
 
 const POCKETBASE_URL = "https://pulpo.cloud";
-const COLLECTION_NAME = "products";
 
 export function getFileUrl(
   recordId: string,
   filename: string,
-  collectionName: string = COLLECTION_NAME
+  collectionName: "products" | "categories"
 ): string {
   if (!filename) return "";
   return `${POCKETBASE_URL}/api/files/${collectionName}/${recordId}/${filename}`;
