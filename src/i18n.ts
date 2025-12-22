30f08f3e09b9950d01da3633612029ca32f03c68
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-22 14:59:18 +0000

    start with language picker

---
+++ src/i18n.ts
@@ -0,0 +1,7 @@
+export const defaultLang = "es";
+export const languages = [
+  { code: "es", label: "Español", flag: "🇪🇸" },
+  { code: "es-ar", label: "Argentino", flag: "🇦🇷" },
+  { code: "en", label: "English", flag: "🇺🇸" },
+  { code: "de", label: "Deutsch", flag: "🇩🇪" },
+];
