9206420289c902802d778840c5be71d10ab4b714
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-23 19:47:55 +0000

    add social media

--- src/collections/Tenant.ts
+++ src/collections/Tenant.ts
@@ -6,6 +6,14 @@ export const TenantSchema = z.object({
   address: z.string(),
   phone: z.string(),
   whatsapp: z.string(),
+  social: z.object({
+    facebook: z.string(),
+    instagram: z.string(),
+    youtube: z.string(),
+    tripadvisor: z.string(),
+    yelp: z.string(),
+    restaurantguru: z.string(),
+  }),
 });
 
 export type Tenant = z.infer<typeof TenantSchema>;
