f8988d923478f701fd184a118820b713d6d826d4
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-25 13:07:39 +0000

    download images

--- src/collections/Product.ts
+++ src/collections/Product.ts
@@ -7,19 +7,9 @@ export const ProductSchema = z.object({
   description: I18nSchema,
   note: I18nSchema,
   price: z.number(),
-  photo: z.string(),
+  image: z.string(),
   allergies: z.array(z.string()),
   category: z.string(),
 });
 
-export const CategorySchema = z.object({
-  id: z.string(),
-  name: I18nSchema,
-  description: I18nSchema,
-  sort: z.number(),
-  photo: z.string().optional(),
-  products: z.array(ProductSchema),
-});
-
-export type Category = z.infer<typeof CategorySchema>;
 export type Product = z.infer<typeof ProductSchema>;
