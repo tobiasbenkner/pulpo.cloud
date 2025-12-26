62b4456deb3766a359cd64e3185233f0f7b71b88
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-26 15:16:41 +0000

    fix pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   image: {
-    domains: ['directus.pulpo.cloud'],
+    domains: ['admin.pulpo.cloud'],
   },
   vite: {
     plugins: [tailwindcss()]
