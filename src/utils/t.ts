8b0abdc763228df4151223f7fde849840ac02691
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-12-28 23:16:57 +0000

    better behavior without lang

--- src/utils/t.ts
+++ src/utils/t.ts
@@ -8,7 +8,7 @@ export type I18n = z.infer<typeof I18nSchema>;
 
 export function t(i18n: I18n | undefined | null, lang: string): string {
   if (!i18n) return "";
-  if (!lang) return "";
+  if (!lang) return i18n.value ?? "";
 
   if (i18n.translations?.[lang]) {
     return i18n.translations[lang];
