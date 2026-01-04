443a7054fe3ea15a636470ca1d4c2d557a2031c1
Tobias Benkner <tobias.benkner@benkner-it.de>
2026-01-04 00:19:46 +0000

    starting with agenda

---
+++ apps/agenda/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+import svelte from '@astrojs/svelte';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [svelte()]
+});
