375b5cf4e2268680bdf2ada2b7781f5247a96f74
Tobias Benkner <tobias.benkner@benkner-it.de>
2026-01-04 00:19:46 +0000

    starting with agenda

---
+++ apps/agenda/src/lib/directus.ts
@@ -0,0 +1,53 @@
+import { createDirectus, rest, realtime, authentication } from "@directus/sdk";
+
+export type Reservation = {
+  id: string;
+  status: string;
+  date: string;
+  time: string;
+  name: string;
+  person_count: number;
+  contact: string;
+  notes: string;
+  was_there: boolean;
+  tenant: string;
+  user: string | User;
+};
+
+export type ReservationSettingTurn = {
+  label: string;
+  start: string;
+  color: string;
+};
+
+export type ReservationSetting = {
+  id: string;
+  tenant: string;
+  turns: ReservationSettingTurn[];
+};
+
+export type User = {
+  id: string;
+  first_name: string;
+  avatar: string;
+  tenant: string;
+};
+
+export type Schema = {
+  reservations: Reservation[];
+  reservations_settings: ReservationSetting[];
+  directus_users: User[];
+};
+
+export function getDirectusClient(token?: string) {
+  const client = createDirectus<Schema>(import.meta.env.DIRECTUS_URL)
+    .with(rest())
+    .with(realtime())
+    .with(authentication("cookie"));
+
+  if (token) {
+    client.setToken(token);
+  }
+
+  return client;
+}
