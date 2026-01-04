143ac6f6e08d5abeb671c3880c4a1670fb5cf27e
Tobias Benkner <tobias.benkner@benkner-it.de>
2026-01-04 18:18:19 +0000

    starting with turbo

---
+++ packages/directus-api/src/directus.ts
@@ -0,0 +1,53 @@
+import { createDirectus, rest, realtime, authentication, type DirectusFile } from "@directus/sdk";
+
+export type Reservation = {
+  id: string;
+  status: string;
+  date: string;
+  time: string;
+  name: string;
+  person_count: number;
+  contact: string;
+  notes: string;
+  was_there: boolean;
+  tenant: string;
+  user: string | User;
+};
+
+export type ReservationSettingTurn = {
+  label: string;
+  start: string;
+  color: string;
+};
+
+export type ReservationSetting = {
+  id: string;
+  tenant: string;
+  turns: ReservationSettingTurn[];
+};
+
+export type User = {
+  id: string;
+  first_name: string | null;
+  avatar: string | DirectusFile;
+  tenant: string;
+};
+
+export type Schema = {
+  reservations: Reservation[];
+  reservations_settings: ReservationSetting[];
+  directus_users: User[];
+};
+
+export function getDirectusClient(token?: string) {
+  const client = createDirectus<Schema>(import.meta.env.PUBLIC_DIRECTUS_URL)
+    .with(rest())
+    .with(realtime())
+    .with(authentication());
+
+  if (token) {
+    client.setToken(token);
+  }
+
+  return client;
+}
