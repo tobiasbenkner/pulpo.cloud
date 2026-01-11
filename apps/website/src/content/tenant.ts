0bcd631ac2f37dc4febe4266ea8bb5fc94031a70
Tobias Benkner <tobias.benkner@benkner-it.de>
2026-01-11 16:34:09 +0100

    add opening hours

--- apps/website/src/content/tenant.ts
+++ apps/website/src/content/tenant.ts
@@ -2,6 +2,9 @@ import { z } from "astro/zod";
 import { directus } from "../lib/directus";
 import { defineCollection } from "astro:content";
 import { readItem } from "@directus/sdk";
+import { I18nSchema } from "../utils/t";
+import { convertI18n } from "./utils";
+import { getDefaultLanguage } from "./language";
 
 const SOCIAL_KEYS = [
   "facebook",
@@ -21,10 +24,41 @@ export const tenant = defineCollection({
     if (!tenantId) {
       throw new Error("TENANT_ID environment variable is missing!");
     }
-    const tenant = await directus.request(readItem("tenants", tenantId));
+
+    const defaultLanguage = await getDefaultLanguage(tenantId);
+
+    const tenant = await directus.request(
+      readItem("tenants", tenantId, {
+        fields: [
+          "*",
+          "opening_hours.*",
+          "opening_hours.translations.*",
+          "opening_hours.translations.languages_id.*",
+        ],
+      })
+    );
     return [
       {
         ...tenant,
+        opening_hours: (tenant?.opening_hours ?? []).map((it: any) => {
+          return {
+            days_label: convertI18n(
+              it?.translations,
+              "days_label",
+              defaultLanguage.code
+            ),
+            hours_text: convertI18n(
+              it?.translations,
+              "hours_text",
+              defaultLanguage.code
+            ),
+            additional_info: convertI18n(
+              it?.translations,
+              "additional_info",
+              defaultLanguage.code
+            ),
+          };
+        }),
         id: "main",
       },
     ];
@@ -46,6 +80,13 @@ export const tenant = defineCollection({
       yelp: z.string().optional(),
       restaurantguru: z.string().optional(),
       tiktok: z.string().optional(),
+      opening_hours: z.array(
+        z.object({
+          days_label: I18nSchema,
+          hours_text: I18nSchema,
+          additional_info: I18nSchema,
+        })
+      ),
     })
     .transform((data) => {
       const social_links = SOCIAL_KEYS.filter((key) => data[key]).map(
