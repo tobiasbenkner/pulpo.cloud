53ac903311c6afb779e7612848a5fadd53552b9f
Tobias Benkner <tobias.benkner@benkner-it.de>
2026-01-12 19:17:06 +0100

    carousel and contact

---
+++ apps/website/src/components/blocks/blocks/Contact.i18n.ts
@@ -0,0 +1,17 @@
+export const translations = {
+  en: {
+    location: "Location",
+    follow: "Follow us",
+    schedule: "Schedule",
+  },
+  es: {
+    location: "Ubicación",
+    follow: "Síguenos",
+    schedule: "Horario",
+  },
+  de: {
+    location: "Standort",
+    follow: "Folge uns",
+    schedule: "Öffnungszeiten",
+  },
+} as const;
