11285a9d3a3dac61ea0f770db5837681c3733e9d
Tobias Benkner <kontakt@tobias-benkner.de>
2026-02-08 22:25:04 +0000

    add svelte and tailwind

--- apps/shop/astro.config.mjs
+++ apps/shop/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [svelte()],
 });
