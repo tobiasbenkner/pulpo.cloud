e7c735afe765324c62dfaf04b51211fc441857de
Tobias Benkner <kontakt@tobias-benkner.de>
2026-03-23 09:23:07 +0100

    add pocketbase

---
+++ apps/pulpo-app/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+
+	_ "github.com/pulpo-cloud/pulpo-app/migrations"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+)
+
+//go:embed all:pb_public
+var publicFS embed.FS
+
+func main() {
+	// Doppelklick ohne Argumente → automatisch "serve" starten
+	if len(os.Args) == 1 {
+		os.Args = append(os.Args, "serve", "--http=0.0.0.0:8090")
+	}
+
+	app := pocketbase.NewWithConfig(pocketbase.Config{
+		DefaultDataDir:            "pb_data",
+		DefaultDev:                version == "dev",
+		HideStartBanner:          false,
+	})
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// Embedded frontends servieren: /shop, /agenda, /settings
+		for _, name := range []string{"shop", "agenda", "settings"} {
+			sub, err := fs.Sub(publicFS, "pb_public/"+name)
+			if err != nil {
+				continue
+			}
+			se.Router.GET("/"+name+"/{path...}", apis.Static(sub, true))
+		}
+
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ apps/pulpo-app/migrations/init.go
@@ -0,0 +1,4 @@
+// Package migrations enthält alle PocketBase Auto-Migrations.
+// Neue Migrations werden generiert via: ./pulpo-app migrate collections
+// Jede Migration wird automatisch beim Start ausgeführt.
+package migrations
