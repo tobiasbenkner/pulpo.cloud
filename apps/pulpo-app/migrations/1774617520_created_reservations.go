4e57065910deb523a9664d5e0da795ecda41b16f
Tobias Benkner <kontakt@tobias-benkner.de>
2026-03-27 13:21:17 +0000

    add tables for agenda

---
+++ apps/pulpo-app/migrations/1774617520_created_reservations.go
@@ -0,0 +1,180 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": "",
+			"deleteRule": "",
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text2862495610",
+					"max": 0,
+					"min": 0,
+					"name": "date",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1872009285",
+					"max": 0,
+					"min": 0,
+					"name": "time",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1579384326",
+					"max": 0,
+					"min": 0,
+					"name": "name",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text703552422",
+					"max": 0,
+					"min": 0,
+					"name": "person_count",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1281549880",
+					"max": 0,
+					"min": 0,
+					"name": "contact",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"convertURLs": false,
+					"hidden": false,
+					"id": "editor18589324",
+					"maxSize": 0,
+					"name": "notes",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "editor"
+				},
+				{
+					"hidden": false,
+					"id": "bool827462018",
+					"name": "arrived",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "bool"
+				},
+				{
+					"cascadeDelete": false,
+					"collectionId": "_pb_users_auth_",
+					"hidden": false,
+					"id": "relation2375276105",
+					"maxSelect": 1,
+					"minSelect": 0,
+					"name": "user",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "relation"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_1473635903",
+			"indexes": [
+				"CREATE INDEX ` + "`" + `idx_nMxpHHcXA3` + "`" + ` ON ` + "`" + `reservations` + "`" + ` (` + "`" + `date` + "`" + `)"
+			],
+			"listRule": "",
+			"name": "reservations",
+			"system": false,
+			"type": "base",
+			"updateRule": "",
+			"viewRule": ""
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1473635903")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
