a03c59275314ccc0ae8edfce7409f84244ab7f40
Tobias Benkner <kontakt@tobias-benkner.de>
2026-04-16 12:02:46 +0100

    better menu style

---
+++ apps/pulpo-app/migrations/1776336101_updated_website_config.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3818476082")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(11, []byte(`{
+			"hidden": false,
+			"id": "file2229311990",
+			"maxSelect": 1,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "favicon",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3818476082")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("file2229311990")
+
+		return app.Save(collection)
+	})
+}
---
+++ apps/pulpo-app/migrations/1776336189_updated_website_config.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3818476082")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(12, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text2916757198",
+			"max": 0,
+			"min": 0,
+			"name": "accent_color",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3818476082")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text2916757198")
+
+		return app.Save(collection)
+	})
+}
